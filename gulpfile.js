062c7bb4ce0ef0e17afb50235f124c8d3bb41baa
timcartwright <tim@websitesbytim.com>
2015-10-25 18:32:09 +0530

    Added Gulp default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,41 @@
+// vars for each module
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee'),
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
+	concat = require('gulp-concat');
 
-gulp.task('log', function(){
-	gutil.log('Workflows are awesome');
+// Sources - can be multiple in each array
+var coffeeSources = ['components/coffee/*.coffee'];
+var jsSources = ['components/scripts/*.js'];
+var sassSources = ['components/sass/main.scss']
 
+// Tasks
+gulp.task('coffee', function(){
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare: true})
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
 });
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+	.pipe(concat('script.js'))
+	.pipe(browserify())
+	.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function() {
+	gulp.src(sassSources)
+	.pipe(compass({
+		sass: 'components/sass',
+		image: 'builds/development/image',
+		style: 'expanded'
+	})
+		.on('error', gutil.log))
+	.pipe(gulp.dest('builds/development/css'))
+});
+
+// Default runs when 'gulp' is called from terminal
+gulp.task('default', ['coffee', 'js', 'compass']);
