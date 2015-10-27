b4c0849289789007aba212c7a61ca701bd0bb964
timcartwright <tim@websitesbytim.com>
2015-10-27 11:21:14 +0530

    Transitions

--- components/scripts/main.js
+++ components/scripts/main.js
@@ -1,1 +1,26 @@
 
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top - 70
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+
+
+	$(window).scroll(function(){
+    if($(window).scrollTop() > $(window).height() - 100 ) {
+       $(".navbar-default").addClass("show-navbar");
+    }
+    else{
+       $(".navbar-default").removeClass("show-navbar");
+    }
+	});
+});
